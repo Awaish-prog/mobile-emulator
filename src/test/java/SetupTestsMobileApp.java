79248df1c7dd593d988d905f37f2e16756da44b5
Awaish-prog <awaishk640@gmail.com>
2024-02-09 03:40:08 +0530

    added test set up

--- src/test/java/SetupTestsMobileApp.java
+++ src/test/java/SetupTestsMobileApp.java
@@ -1,11 +1,13 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
 import java.util.concurrent.TimeUnit;
 
 import java.net.MalformedURLException;
@@ -20,8 +22,8 @@ public class SetupTestsMobileApp {
     public static AppiumDriver getAppDriver() throws MalformedURLException {
         if(driver == null){
             DesiredCapabilities openFabricAppCapabilities = new DesiredCapabilities();
-            openFabricAppCapabilities.setCapability("platformName", "Android");
-            openFabricAppCapabilities.setCapability("deviceName", "Android");
+            openFabricAppCapabilities.setCapability(CapabilityType.PLATFORM_NAME, Platform.ANDROID);
+//            openFabricAppCapabilities.setCapability("deviceName", "Android");
             openFabricAppCapabilities.setCapability("app", System.getProperty("user.dir") + "/app-debug.apk");
             openFabricAppCapabilities.setCapability("automationName", "UiAutomator2");
             driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), openFabricAppCapabilities);
